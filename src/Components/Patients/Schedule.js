6ea8c3c9a4d6593937aab517560a19e188743ac8
Pedro Miguel Marques Rodrigues <pedrommarquesrodrigues@gmail.com>
2023-10-05 12:03:22 +0100

    eddit on function to prevent default

--- src/Components/Patients/Schedule.js
+++ src/Components/Patients/Schedule.js
@@ -62,6 +62,10 @@ const Schedule = (props) => {
     const handleKeyDown = (event) => {
       if (event.key === "Escape") {
         props.onClosing();
+        console.log("cancel test");
+      } else if (event.key === "Enter") {
+        event.preventDefault();
+        console.log(test);
       }
     };
 
